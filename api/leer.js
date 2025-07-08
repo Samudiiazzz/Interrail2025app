2d6292c7a6026e0f32d3bffe73f41afe44786912
Samudiiazzz <samdia4@gmail.com>
2025-07-08 18:44:43 +0200

    Update leer.js

--- api/leer.js
+++ api/leer.js
@@ -1,7 +1,11 @@
-import { redis } from '../lib/redis'
+import { redis } from '../lib/redis.js'
 
 export default async function handler(req, res) {
   try {
+    // Imprime las variables de entorno
+    console.log("URL:", process.env.UPSTASH_REDIS_REST_URL);
+    console.log("TOKEN:", process.env.UPSTASH_REDIS_REST_TOKEN);
+
     const data = await redis.get("itinerario_global");
     let itinerary = [];
     if (data) {
@@ -13,7 +17,7 @@ export default async function handler(req, res) {
     }
     res.status(200).json({ itinerary });
   } catch (error) {
-    console.error(error);
+    console.error("ERROR EN HANDLER:", error);
     res.status(500).json({ error: "No se pudo leer el itinerario", itinerary: [] });
   }
 }
